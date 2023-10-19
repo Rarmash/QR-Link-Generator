6fd88b88c1e5301f7efb565e7edc33a80121e120
Andrew <62994051+Rarmash@users.noreply.github.com>
2023-10-19 12:45:48 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     id("java")
 }
 
-group = "org.example"
-version = "1.0-SNAPSHOT"
+group = "com.rarmash.qr_link_generator"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
 }
 
